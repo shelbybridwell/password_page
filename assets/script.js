4fa108e6f32f2b982c920b4be23a36a1c549a86d
Shelby Bridwell <shelbybridwell@Shelbys-MacBook-Pro.local>
2022-08-17 23:57:20 -0500

    tried new code, still not working

--- assets/script.js
+++ assets/script.js
@@ -5,13 +5,18 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+function createPassword(length) {
+  let password = '';
+  const chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let passwordLength = length;
 
-  passwordText.value = password;
+  const array = new Uint32Array(length);
+  window.crypto.getRandomValues(array);
 
+  for (i=0; i<length; i++) {
+    password += chars[array[i] % chars.length];
+  }
 }
-
+createPassword(5)
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
