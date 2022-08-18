3fe387e41b781e2dce4aecb2fdfc091a07b4648a
Shelby Bridwell <shelbybridwell@Shelbys-MacBook-Pro.local>
2022-08-18 01:36:52 -0500

    forgot to add special characters, still need to figure out prompts

--- assets/script.js
+++ assets/script.js
@@ -2,12 +2,14 @@ function generatePassword(passwordLength) {
   var numberChars = "0123456789";
   var upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var lowerChars = "abcdefghijklmnopqrstuvwxyz";
+  var specialChars = "!@$%^&*()";
   var allChars = numberChars + upperChars + lowerChars;
   var randPasswordArray = Array(passwordLength);
   randPasswordArray[0] = numberChars;
   randPasswordArray[1] = upperChars;
   randPasswordArray[2] = lowerChars;
-  randPasswordArray = randPasswordArray.fill(allChars, 3);
+  randPasswordArray[3] = specialChars;
+  randPasswordArray = randPasswordArray.fill(allChars, 4);
   return shuffleArray(randPasswordArray.map(function(x) { return x[Math.floor(Math.random() * x.length)] })).join('');
 }
 
@@ -21,7 +23,11 @@ function shuffleArray(array) {
   return array;
 }
 document.getElementById("generate").addEventListener("click", myFunction);
-
+ // var numResult = prompt("Enter a name you want to be called");
+  //var uppercharResult = prompt("Inlude captitol letters? ")
+ // var lowercharResult = prompt("Inlude captitol letters? ")
+ // var specialcharResult = prompt("Inlude captitol letters? ")
+ //  if yes ()
 function myFunction() {
   document.getElementById("password").innerHTML =  (generatePassword(10));
 }
